e2d2bf4ab7a584c1df558a9b7b79047ad39361a2
Sergey Vojtovich <svoj@mariadb.org>
2018-10-19 23:13:47 +0400

    MDEV-16686 - Assertion `(*tables)->reginfo.lock_type >= TL_READ' failed              in lock_external upon using a mix of trigger, view, SP
    Prevent TL_IGNORE tables from being added to the SP's m_sptabs hash.
    
    Test cases merged from MySQL rev 41c5f4a0235ac9375080c4c79d207fe429e94a2c
    BUG#19988193: ASSERTION `(*TABLES)->REGINFO.LOCK_TYPE >= TL_READ'
                  FAILED IN LOCK_EXTERNAL
    BUG#21198646: ASSERTION FAILED: (*TABLES)->REGINFO.LOCK_TYPE >=
                  TL_READ FILE LOCK.CC, LINE 356

--- sql/sp_head.cc
+++ sql/sp_head.cc
@@ -4106,7 +4106,7 @@ sp_head::merge_table_list(THD *thd, TABLE_LIST *table, LEX *lex_for_tmp_check)
   }
 
   for (; table ; table= table->next_global)
-    if (!table->derived && !table->schema_table)
+    if (!table->derived && !table->schema_table && table->lock_type != TL_IGNORE)
     {
       /*
         Structure of key for the multi-set is "db\0table\0alias\0".
